ff51a7d38d9b874b477f945a6b22c5503cd72694
alireza karbalayi morad <alireza.version@gmail.com>
2023-04-30 16:01:17 +0330

    leaflet package deleted but react-leaflet is exist and map works properly but without pin icon

--- pages/LeafletMap.js
+++ pages/LeafletMap.js
@@ -1,15 +1,9 @@
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet'
+
 
 const LeafletMap = () => {
-  const CustomMarkerIcon = L.icon({
-    iconUrl: 'https://www.gov.br/mre/pt-br/consulado-bruxelas/pin.png/@@images/1592a7e9-c806-4c61-8d86-5455ad76f496.png',
-    iconSize: [40, 40],
-    iconAnchor: [15, 30],
-    popupAnchor: [0, -30],
-    className: 'custom-marker-icon', // You can add custom CSS classes here
-  });
+
   return (
     <MapContainer
       center={[35.72550, 51.38623]}
@@ -20,7 +14,7 @@ const LeafletMap = () => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 
       />
-      <Marker position={[35.72550, 51.38623]} icon={CustomMarkerIcon} ></Marker>
+      <Marker position={[35.72550, 51.38623]} icon={}  ></Marker>
     </MapContainer>
   );
 };
